e3d7366e8e1f483cc3d1d5909b3c60b73004d364
Erik Garrison <erik.garrison@gmail.com>
2019-09-17 10:33:23 +0200

    implement shadow function to build interval set during transclosure

--- src/transclosure.cpp
+++ src/transclosure.cpp
@@ -17,7 +17,6 @@ size_t compute_transitive_closures(
     std::ofstream seq_v_out(seq_v_file.c_str());
     // remember the elements of Q we've seen
     sdsl::bit_vector q_seen_bv(seqidx.seq_length());
-    //size_t seq_bytes = 0;
     // for each base in seqidx
     //   collect mapped bases
     //   mark our seen bitvector
@@ -36,7 +35,50 @@ size_t compute_transitive_closures(
                   << " position offset " << offset(pos) << (is_rev(pos)?"-":"+") << std::endl;
     }
     */
+    // a buffer of ranges to write into our iitree, arranged by range ending position in Q
+    // we flush those intervals that don't get extended into the next position in S
+    //
+    // this maps from a position in Q (our input seqs concatenated, offset and orientation)
+    // to a range (start and length) in S (our graph sequence vector)
+    std::map<pos_t, std::pair<uint64_t, uint64_t>> range_buffer;
+    // here we try to find a growing range to extend
+    auto extend_range = [&](const uint64_t& s_pos, const pos_t& q_pos) {
+        // find a position in the map that we can add onto
+        pos_t q_last_pos = q_pos;
+        decr_pos(q_last_pos);
+        auto f = range_buffer.find(q_last_pos);
+        // if one doesn't exist, add the range
+        if (f == range_buffer.end()) {
+            range_buffer[q_pos] = std::make_pair(s_pos, 1);
+        } else {
+            // if one does, we expand the range and drop it back into the map at the new Q end pos
+            std::pair<uint64_t, uint64_t> x = f->second;
+            range_buffer.erase(f);
+            ++x.second; // increment the match length
+            range_buffer[q_pos] = x; // and stash it
+        }
+    };
+    auto flush_ranges = [&](const uint64_t& s_pos) {
+        // for each range, we're going to see if we've stepped more than one past the end
+        // if we have, we'll write them out
+        std::map<pos_t, std::pair<uint64_t, uint64_t>>::iterator it = range_buffer.begin();
+        while (it != range_buffer.end()) {
+            if (it->second.first + it->second.second != s_pos) {
+                // flush
+                std::cerr << "would flush " << offset(it->first) << ":" << (is_rev(it->first) ? "-" : "+")
+                          << " -> " << it->second.first << ":" << it->second.second << std::endl;
+                it = range_buffer.erase(it);
+            } else {
+                ++it;
+            }
+        }
+    };
     //std::cerr << "input seq len " << input_seq_length << std::endl;
+    // new way, accumulate pairs of ranges in S (output seq of graph) and Q (input seqs concatenated)
+    // but how
+    // maybe we set up a handler that decides when to flush things
+    // we flush when we stop extending
+    // we determine when we stop extending when we have stepped a bp and broke our range extension
     for (uint64_t i = 1; i <= input_seq_length; ++i) {
         //std::cerr << q_seen_bv << std::endl;
         if (q_seen_bv[i-1]) continue;
@@ -44,12 +86,13 @@ size_t compute_transitive_closures(
         char base = seqidx.at(i-1);
         seq_v_out << seqidx.at(i-1);
         size_t seq_v_length = seq_v_out.tellp();
-        // mark current
+        uint64_t flushed = range_buffer.size();
+        flush_ranges(seq_v_length);
+        flushed -= range_buffer.size();
+        std::cerr << "seq " << seq_v_length << " " << base << " buf size " << range_buffer.size() << " flushed " << flushed << std::endl;
+        // mark current 
         q_seen_bv[i-1] = 1;
         // emit current
-        //node_mm.append(i, make_pos_t(i,false));
-        //path_mm.append(i, make_pos_t(i,false));
-        //std::cerr << "closure " << i << " to " << seq_v_length << std::endl;
         std::set<std::pair<pos_t, uint64_t>> todo;
         std::unordered_map<uint64_t, uint64_t> seen_seqs;
         todo.insert(std::make_pair(make_pos_t(i, false), min_transclose_len));
@@ -57,16 +100,11 @@ size_t compute_transitive_closures(
         while (!todo.empty()) {
             pos_t j = todo.begin()->first;
             uint64_t match_len = todo.begin()->second;
-            /*
-            std::cerr << seq_v_length << "\t"
-                      << offset(j) << "\t"
-                      << (is_rev(j)?"-":"+") << std::endl;
-            */
             todo.erase(todo.begin());
-            //std::cerr << "todo size " << todo.size() << std::endl;
             //assert(q_seen_bv[offset(j)-1]==1);
             node_mm.append(seq_v_length, j);
             path_mm.append(offset(j), make_pos_t(seq_v_length,is_rev(j)));
+            extend_range(seq_v_length, j);
             // todo rip this out
             if (min_transclose_len && match_len < min_transclose_len) {
                 continue;
@@ -81,28 +119,11 @@ size_t compute_transitive_closures(
                 pos_t pos = aln_iitree.data(s);
                 //std::cerr << " with overlap " << start << "-" << end << std::endl;
                 //std::cerr << "and position offset " << offset(pos) << (is_rev(pos)?"-":"+") << std::endl;
-                // if it's long enough, include it
-                // todo, use paramater for local smoothing
-                // find the position in the match
-                /*
-                if (is_rev(pos)) {
-                    incr_pos(pos, end - n);
-                    std::cerr << "after pos increment " << offset(pos) << std::endl;
-                } else {
-
-                }
-                */
                 //std::cerr << "n " << n << " start " << start << std::endl;
                 incr_pos(pos, n - start);
                 uint64_t k = offset(pos);
-                //std::cerr << "got k " << k << " " << base << " ==? " << seqidx.at_pos(pos) << std::endl;
-                // TODO check that the base is right --- in any case this is done at the end of the process when checking paths
-                //assert(base == seqidx.at_pos(pos));
-                //assert(dna_reverse_complement(base) == seqidx.at_pos(pos));
                 if (k && !q_seen_bv[k-1]) {
-                    //std::cerr << "closing " << k << std::endl;
                     uint64_t seq_id = seqidx.seq_id_at(offset(pos));
-                    //std::cerr << "seq id " << seq_id << std::endl;
                     auto& c = seen_seqs[seq_id];
                     if (!repeat_max || c < repeat_max) {
                         ++c;
@@ -112,15 +133,11 @@ size_t compute_transitive_closures(
                 }
             }
         }
-        /*
-        for (auto& c : seen_seqs) {
-            std::cerr << "seen " << c.first << " " << c.second << std::endl;
-        }
-        */
     }
     // close the graph sequence vector
     size_t seq_bytes = seq_v_out.tellp();
     seq_v_out.close();
+    flush_ranges(seq_bytes+2);
     // build node_mm and path_mm indexes
     node_mm.index(seq_bytes);
     path_mm.index(input_seq_length);
