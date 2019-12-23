24b1b1f2fdac574dd6d6d7e19fb17208723840ab
Erik Garrison <erik.garrison@gmail.com>
2019-12-23 13:27:34 +0100

    big jump, getting ready for fully parallel transclosure

--- src/transclosure.hpp
+++ src/transclosure.hpp
@@ -7,6 +7,7 @@
 #include <unordered_set>
 #include <set>
 #include "sdsl/bit_vectors.hpp"
+#include "atomic_bitvector.hpp"
 #include "seqindex.hpp"
 #include "mmiitree.hpp"
 #include "pos.hpp"
@@ -14,6 +15,31 @@
 
 namespace seqwish {
 
+void extend_range(const uint64_t& s_pos,
+                  const pos_t& q_pos,
+                  std::map<pos_t, std::pair<uint64_t, uint64_t>>& range_buffer);
+
+void flush_ranges(const uint64_t& s_pos,
+                  std::map<pos_t, std::pair<uint64_t, uint64_t>>& range_buffer,
+                  mmmulti::iitree<uint64_t, pos_t>& node_iitree,
+                  mmmulti::iitree<uint64_t, pos_t>& path_iitree);
+
+void for_each_fresh_range(const match_t& range,
+                          bool mark_bv,
+                          atomicbitvector::atomic_bv_t& seen_bv,
+                          const std::function<void(match_t)>& lambda);
+
+void handle_range(match_t s,
+                  const uint64_t& query_start,
+                  const uint64_t& query_end,
+                  std::vector<std::pair<match_t, bool>>& ovlp,
+                  std::set<std::pair<pos_t, uint64_t>>& todo);
+
+void explore_overlaps(const match_t& b,
+                      atomicbitvector::atomic_bv_t& seen_bv,
+                      mmmulti::iitree<uint64_t, pos_t>& aln_iitree,
+                      std::vector<std::pair<match_t, bool>>& ovlp,
+                      std::set<std::pair<pos_t, uint64_t>>& todo);
 
 size_t compute_transitive_closures(
     seqindex_t& seqidx,
