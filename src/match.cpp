a4033351be31d0faf6b0046255b12037ca2a9f8d
Erik Garrison <erik.garrison@gmail.com>
2020-01-03 22:52:23 +0100

    correct a logic error with todo bounding

--- src/match.cpp
+++ src/match.cpp
@@ -6,4 +6,12 @@ match_t get_match(mmmulti::iitree<uint64_t, pos_t>& iitree, uint64_t idx) {
     return { iitree.start(idx), iitree.end(idx), iitree.data(idx) };
 }
 
+bool operator<(const match_t& a, const match_t& b) {
+    return a.start < b.start && a.end < b.end && a.pos < b.pos;
+}
+
+bool operator==(const match_t& a, const match_t& b) {
+    return a.start == b.start && a.end == b.end && a.pos == b.pos;
+}
+
 }
