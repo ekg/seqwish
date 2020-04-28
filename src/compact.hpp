ca36f8bb76f5ebdf715882e37c0236cef27a4024
Erik Garrison <erik.garrison@gmail.com>
2020-04-28 18:39:07 +0200

    compact in parallel

--- src/compact.hpp
+++ src/compact.hpp
@@ -1,11 +1,12 @@
-#ifndef COMPACT_HPP_INCLUDED
-#define COMPACT_HPP_INCLUDED
+#pragma once
 
 #include <vector>
 #include "sdsl/bit_vectors.hpp"
+#include "atomic_bitvector.hpp"
 #include "seqindex.hpp"
 #include "mmiitree.hpp"
 #include "pos.hpp"
+#include "paryfor.hpp"
 
 namespace seqwish {
 
@@ -15,8 +16,7 @@ void compact_nodes(
     size_t graph_size,
     mmmulti::iitree<uint64_t, pos_t>& node_iitree,
     mmmulti::iitree<uint64_t, pos_t>& path_iitree,
-    sdsl::bit_vector& seq_id_bv);
+    sdsl::bit_vector& seq_id_bv,
+    uint64_t num_threads);
 
 }
-
-#endif
