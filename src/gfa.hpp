8ae187a23769c55df17bc787de5353881b975889
Erik Garrison <erik.garrison@gmail.com>
2020-04-28 18:50:52 +0200

    keep removing reference to threads

--- src/gfa.hpp
+++ src/gfa.hpp
@@ -21,6 +21,7 @@ void emit_gfa(std::ostream& out,
               const sdsl::sd_vector<>::rank_1_type& seq_id_cbv_rank,
               const sdsl::sd_vector<>::select_1_type& seq_id_cbv_select,
               seqindex_t& seqidx,
-              mmmulti::set<std::pair<pos_t, pos_t>>& link_mmset);
+              mmmulti::set<std::pair<pos_t, pos_t>>& link_mmset,
+              const uint64_t& num_threads);
 
 }
