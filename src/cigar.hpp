85be19d73caa43a8165bc5aaa01fe45dfd633911
AndreaGuarracino <andreaguarracino@outlook.com>
2023-06-13 14:36:24 +0200

    make newer `gcc` happy

--- src/cigar.hpp
+++ src/cigar.hpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <iostream>
 #include <sstream>
+#include <cstdint>
 
 namespace seqwish {
 
--- src/utils.hpp
+++ src/utils.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <math.h>
+#include <cstdint>
 
 namespace seqwish {
     double handy_parameter(const std::string& value, const double default_value);
