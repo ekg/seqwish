d8eb5af0e14ede30fc33065a5aa264aad519c243
Erik Garrison <erik.garrison@gmail.com>
2025-11-04 07:27:50 -0600

    Step 19: Remove redundant C++ implementations from exists, time, and utils
    The headers already have inline implementations calling Rust FFI:
    - exists.hpp: file_exists() calls ::file_exists()
    - time.hpp: time_since_epoch_ms() calls ::time_since_epoch_ms()
    - utils.hpp: handy_parameter() calls ::handy_parameter()
    
    Removed the redundant C++ implementations from the .cpp files.

--- src/exists.cpp
+++ src/exists.cpp
@@ -1,10 +1,3 @@
 #include "exists.hpp"
-#include "seqwish_rs.h"
 
-namespace seqwish {
-
-bool file_exists(const std::string& name) {
-    return ::file_exists(name.c_str());
-}
-
-}
+// Functions moved to header as inline functions calling Rust FFI
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,9 +1,3 @@
-#include <string>
 #include "utils.hpp"
-#include "seqwish_rs.h"
 
-namespace seqwish {
-    double handy_parameter(const std::string& value, const double default_value) {
-        return ::handy_parameter(value.c_str(), default_value);
-    }
-}
+// Functions moved to header as inline functions calling Rust FFI
